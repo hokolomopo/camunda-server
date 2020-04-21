4522112ce2206d187d71defff2c30a540020f4d4
hokolomopo <adrien.minne@outlook.com>
2020-04-21 15:47:25 +0200

    Change token format to only post request to Camunda

--- src/test/java/be/yelido/camundaserver/BPMNProcessTest.java
+++ src/test/java/be/yelido/camundaserver/BPMNProcessTest.java
@@ -3,19 +3,16 @@ package be.yelido.camundaserver;
 import be.yelido.camunda.module.data.dto.Execution;
 import be.yelido.camunda.module.data.dto.ProcessInstance;
 import be.yelido.camunda.module.data.dto.Variable;
-import be.yelido.camunda.module.data.request.CamundaType;
-import be.yelido.camunda.module.data.request.ExecutionQuery;
-import be.yelido.camunda.module.data.request.MessageCorrelationParameters;
-import be.yelido.camunda.module.data.request.ProcessInstanceQuery;
+import be.yelido.camunda.module.data.request.*;
 import be.yelido.camunda.module.rest.CamundaRestTemplate;
+import be.yelido.camunda.module.util.CamundaMonitor;
+import be.yelido.camunda.module.util.CamundaToken;
+import be.yelido.camunda.module.util.VariableUtil;
 import jdk.nashorn.internal.parser.Token;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -25,17 +22,19 @@ public class BPMNProcessTest {
 
     @Test
     void runUploadAuditableProcess() throws InterruptedException {
-        CamundaRestTemplate restTemplate = new CamundaRestTemplate(url);
+        CamundaMonitor camundaMonitor = new CamundaMonitor(url);
         MessageCorrelationParameters messageCorrelationParameters;
+        String tmpBK;
 
         // Create Process Instance
-        ProcessInstance p = restTemplate.startProcessInstanceByKey("Upload_Auditable", null);
+        StartProcessInstanceParameters startProcessInstanceParameters = new StartProcessInstanceParameters(UUID.randomUUID().toString(), null);
+        ProcessInstance p = camundaMonitor.getRestTemplate().startProcessInstanceByKey("Upload_Auditable", startProcessInstanceParameters);
         assertNotNull(p);
-        CamundaToken token = new CamundaToken(p.getId(), p.getId());
+        CamundaToken token = new CamundaToken(startProcessInstanceParameters.getBusinessKey());
 
         // Send message
-        messageCorrelationParameters = new MessageCorrelationParameters("UploadServiceImpl_uploadAuditable_msg", token.getCurrentProcessId(), null);
-        restTemplate.sendMessage(messageCorrelationParameters);
+        messageCorrelationParameters = new MessageCorrelationParameters("UploadServiceImpl_uploadAuditable_msg", null);
+        camundaMonitor.correlateMessage(token, messageCorrelationParameters);
 
         //-----------------------------------------
         //Send msg to MQ => start a new sub process
@@ -43,51 +42,39 @@ public class BPMNProcessTest {
         Thread.sleep(100);
 
         // Send message
-        messageCorrelationParameters = new MessageCorrelationParameters("SynchroController_integrateDataSource_msg", token.getCurrentProcessId(), null);
-        restTemplate.sendMessage(messageCorrelationParameters);
-        Thread.sleep(100);
+        tmpBK = UUID.randomUUID().toString();
+        HashMap<String, Variable> vars = VariableUtil.mapFromSingleVariable("BK_JIOUpload", new Variable(tmpBK, CamundaType.STRING));
+        messageCorrelationParameters = new MessageCorrelationParameters("SynchroController_integrateDataSource_msg", vars);
+        camundaMonitor.correlateMessage(token, messageCorrelationParameters);
 
-        // Find newly created sub process
-        ProcessInstanceQuery query = ProcessInstanceQuery.createQuery()
-                .superProcessInstance(token.getCurrentProcessId())
-                .processDefinitionKey("JIO_Upload")
-                .build();
-        ProcessInstance jioUpload = restTemplate.searchProcessInstancesSingleResult(query);
-        token.setCurrentProcessId(jioUpload.getId());
+        token = new CamundaToken(tmpBK);
+        Thread.sleep(100);
 
         //-----------------------------------------
         // JIO_UPLOAD sub-process
         //-----------------------------------------
-        messageCorrelationParameters = new MessageCorrelationParameters("JioUploadIntegrationProcessor_process_mdg", token.getCurrentProcessId(), null);
-        restTemplate.sendMessage(messageCorrelationParameters);
+        messageCorrelationParameters = new MessageCorrelationParameters("JioUploadIntegrationProcessor_process_mdg", null);
+        camundaMonitor.correlateMessage(token, messageCorrelationParameters);
         Thread.sleep(100);
 
-        messageCorrelationParameters = new MessageCorrelationParameters("IntegrationController_integrate_msg", token.getCurrentProcessId(), null);
-        restTemplate.sendMessage(messageCorrelationParameters);
+        messageCorrelationParameters = new MessageCorrelationParameters("IntegrationController_integrate_msg", null);
+        camundaMonitor.correlateMessage(token, messageCorrelationParameters);
 
         Thread.sleep(50);
+        
+        tmpBK = UUID.randomUUID().toString();
+        HashMap<String, Variable> variableMap = VariableUtil.mapFromSingleVariable("BK_Dasys", new Variable(tmpBK, CamundaType.STRING));
+        messageCorrelationParameters = new MessageCorrelationParameters("IntegrationController_SendToDasys_msg", variableMap);
+        camundaMonitor.correlateMessage(token, messageCorrelationParameters);
 
-//        messageCorrelationParameters = new MessageCorrelationParameters("msg", token.getCurrentProcessId(), null);
-//        restTemplate.sendMessage(messageCorrelationParameters);
-        System.out.println(token.getCurrentProcessId());
+        CamundaToken tokenDasys = new CamundaToken(tmpBK);
 
-        HashMap<String, Variable> variableMap = new HashMap<String, Variable>();
-        variableMap.put("BK_Dasys", new Variable("MyBK_Dasys", CamundaType.STRING));
-        messageCorrelationParameters = new MessageCorrelationParameters("IntegrationController_SendToDasys_msg", token.getCurrentProcessId(), variableMap);
-        restTemplate.sendMessage(messageCorrelationParameters);
+        tmpBK = UUID.randomUUID().toString();
+        HashMap<String, Variable> variableMap1 = VariableUtil.mapFromSingleVariable("BK_Legacy", new Variable(tmpBK, CamundaType.STRING));
+        messageCorrelationParameters = new MessageCorrelationParameters("IntegrationController_SendToLegacy_msg", variableMap1);
+        camundaMonitor.correlateMessage(token, messageCorrelationParameters);
 
-
-        HashMap<String, Variable> variableMap1 = new HashMap<String, Variable>();
-        variableMap1.put("BK_Legacy", new Variable("ThatsMyBK", CamundaType.STRING));
-        messageCorrelationParameters = new MessageCorrelationParameters("IntegrationController_SendToLegacy_msg", token.getCurrentProcessId(), variableMap1);
-        restTemplate.sendMessage(messageCorrelationParameters);
-
-        Thread.sleep(100);
-
-        ProcessInstanceQuery pInstanceQuery1 = ProcessInstanceQuery.createQuery().active(true).superProcessInstance(token.getSuperProcessId()).businessKey("ThatsMyBK").build();
-        p = restTemplate.searchProcessInstancesSingleResult(pInstanceQuery1);
-        System.out.println(token.getSuperProcessId());
-        CamundaToken tokenLegacy = new CamundaToken(p.getId(), token.getSuperProcessId());
+        CamundaToken tokenLegacy = new CamundaToken(tmpBK);
 
         //-----------------------------------------
         // ESB Notifs Legacy
@@ -97,28 +84,26 @@ public class BPMNProcessTest {
         HashMap<String, Variable> variableMap2 = new HashMap<String, Variable>();
         variableMap2.put("headerDestination", new Variable(null, CamundaType.STRING));
         variableMap2.put("headerProcessor", new Variable("LEGACY", CamundaType.STRING));
-        messageCorrelationParameters = new MessageCorrelationParameters("ESBNotificationProcessor_process_msg", tokenLegacy.getCurrentProcessId(), variableMap2);
-        restTemplate.sendMessage(messageCorrelationParameters);
+        messageCorrelationParameters = new MessageCorrelationParameters("ESBNotificationProcessor_process_msg", variableMap2);
+        camundaMonitor.correlateMessage(tokenLegacy, messageCorrelationParameters);
 
         Thread.sleep(100);
 
         HashMap<String, Variable> variableMap3 = new HashMap<>();
-        ArrayList<String> BKs = new ArrayList<>(); BKs.add("BK1"); BKs.add("BK2");
+        ArrayList<String> BKs = new ArrayList<>(); BKs.add(UUID.randomUUID().toString()); BKs.add(UUID.randomUUID().toString());
         variableMap3.put("restCallsBKs", VariableUtil.createVariableFromCollection(BKs));
-        messageCorrelationParameters = new MessageCorrelationParameters("ESBNotificationProcessor_end_msg", tokenLegacy.getCurrentProcessId(), variableMap3);
-        restTemplate.sendMessage(messageCorrelationParameters);
-        System.out.println(tokenLegacy.getCurrentProcessId());
+        messageCorrelationParameters = new MessageCorrelationParameters("ESBNotificationProcessor_end_msg", variableMap3);
+        camundaMonitor.correlateMessage(tokenLegacy, messageCorrelationParameters);
 
         Thread.sleep(100);
 
         for (String bk : BKs){
             // Find newly created sub process
             ProcessInstanceQuery restInstanceQuery = ProcessInstanceQuery.createQuery()
-                    .superProcessInstance(tokenLegacy.getSuperProcessId())
                     .businessKey(bk)
                     .processDefinitionKey("Orchestrator_Rest")
                     .build();
-            ProcessInstance restCallInstance = restTemplate.searchProcessInstancesSingleResult(restInstanceQuery);
+            ProcessInstance restCallInstance = camundaMonitor.getRestTemplate().searchProcessInstancesSingleResult(restInstanceQuery);
             assertNotNull(restCallInstance);
         }
     }
